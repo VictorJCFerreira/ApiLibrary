a127a32e083e893527117582d9ddf3739d2eb93a
Victor José de Carvalho Ferreira <victorjcferreira462@gmail.com>
2023-01-04 20:14:55 -0300

    Update biblioteca.js

--- biblioteca.js
+++ biblioteca.js
@@ -1,7 +1,5 @@
 console.log("esssa porra nao ta pegando nem a pau")
 
-console.log("NAO VAAAAAAAAIIIII ESSSE CARAAAAAAAAAAAALHHHHHHOOOOOOOOOO")
-
 console.log("BOOOORAAAAAAAAAAAAAAAAAAAAAA")
 
 console.log("NAO VAAAAAAAIIIII")
